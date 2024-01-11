52f2de15869e15a139d7ed9b574e7415571bdec6
Kakashimusic <154975507+Kakashimusic@users.noreply.github.com>
2024-01-11 14:14:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Rajasahab3891",
+    "https://github.com/Kakashimusic/KakashiXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
